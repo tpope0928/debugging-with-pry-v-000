9f15e1d998ecfaebaef8f3e2620b425958b01960
Thomas Pope <tpope0928@gmail.com>
2018-08-01 21:43:33 +0000

    automatically backed up by learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -6,7 +6,7 @@ def prying_into_the_method
 	puts "We're about to stop because of pry!"
 	
 	def plus_two(num)
-	  num + 2
+	  2 + num
 	end
 	
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!" 
